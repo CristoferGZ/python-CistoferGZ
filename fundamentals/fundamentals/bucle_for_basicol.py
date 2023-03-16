fed43143fd7924a4d4eb20fd78c34f42e35d7cc8
CristoferGZ <cristofer.gutierrez,cerda@alumnos.sip.cl>
2023-03-16 12:52:02 -0300

    bucle

---
+++ fundamentals/fundamentals/bucle_for_basicol.py
@@ -0,0 +1,21 @@
+
+
+'''
+for i in range(0,150):
+	  print(i)
+for i in range(0,100):
+	if i % 10 ==0:
+		print("coding dojo")
+	elif i % 5 ==0:
+		print("coding")
+	else:
+		print(i)
+
+	total=0
+for i in range(1,500000,2):
+	total +=i
+print(total)
+'''
+
+for i in range(2018, 0, -4):	
+	print(i)
