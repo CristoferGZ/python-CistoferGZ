6811023149ba906fe83d4824a2fe4ef5616103f7
CristoferGZ <cristofer.gutierrez,cerda@alumnos.sip.cl>
2023-03-22 12:38:52 -0300

    funcion

---
+++ fundamentals/_functions_basic_i/functions_basic_i/functions_basic_i.py
@@ -0,0 +1,113 @@
+#1 imprimira el numero de los grupos y entregara como resultado
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+
+
+#2 hay un error de sintaxis 
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+
+
+#3 imrpimira el numero de libros que hay en la reserva de la escuela
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+
+
+#4 imprime el numero de dedos y suma el resultado de las dos manos lo que dara 10
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+
+
+#5 imprime el numero de los lagos grandes 
+
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+
+
+#6 imprimira la suma de los dos numeros , pero la funcion no se sumara por que no hay return
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+
+
+#7 imprimira la suma de los numeros sumando las letras ya que a diferencia
+def concatenate(b,c):
+    return str(b)+str(c)
+print(concatenate(2,5))
+
+
+#8 imprimira el numero de oceanos que rodean al continente si menor 
+#  que b retornara 5 si b es mayor retornara 10
+def number_of_oceans_or_fingers_or_continents():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(number_of_oceans_or_fingers_or_continents())
+
+
+#9  imprimera la funcion y si b es menor que c retornara 7 y si es mayor retornara 14 
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+
+
+#10 rd una adicion de 3 y 5
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+
+
+#11 es una adicion b 500 imprimira 300
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+print(b)
+foobar()
+print(b)
+
+
+#12 b es igual a 500 500  300 500 retornara a b
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+
+
+#13 b es igual a 500 y la funcion de foobar si es 300 retornara a b
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+
+#JGKJHKJHSJHS
