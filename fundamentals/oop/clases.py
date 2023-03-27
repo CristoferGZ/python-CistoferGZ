936c66b48f775a8f2f1c7f999bd613c894c6ad25
CristoferGZ <cristofer.gutierrez,cerda@alumnos.sip.cl>
2023-03-27 14:26:55 -0300

    intermedia

---
+++ fundamentals/oop/clases.py
@@ -0,0 +1,13 @@
+class Usuario:
+    def __init__(self) -> None:
+        self.name = "michael"
+        self.email = "michael@codingdojo.com"
+        self.balance_cuenta =0
+
+Usuario()
+guido =Usuario()
+monty =Usuario()
+
+print(guido.name)
+print(monty.name)
+guido.name =
